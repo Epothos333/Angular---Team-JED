89a4958e3a6a5a37567763c639cb5163dc652deb
Edwin A <ea976@nyu.edu>
2016-03-07 09:36:26 -0500

    Solved ngRoute problem finally

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var app = angular.module('jedModule', ['ngRoute']);
 
-app.config(function($routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl: 'index.html'
-	});
-	$routeProvider.when('/view1', {
-		controller: 'controllerOne',
-		templateUrl: 'view1.html'
-	});
-	$routeProvider.when('/view2', {
-		controller: 'controllerTwo',
-		templateUrl: 'view2.html'
-	});
-});
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/view1', {
+        templateUrl: 'view1.html',
+        controller: 'controllerOne'
+      }).
+      when('/view2', {
+        templateUrl: 'view2.html',
+        controller: 'controllerTwo'
+      });
+  }]);
