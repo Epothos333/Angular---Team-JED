bcc4b2787c4dbbdaf8d6a885f5369a1b2123a3cd
Edwin A <ea976@nyu.edu>
2016-03-08 17:09:34 -0500

    Reddit Titles added to View 1

--- controllerOne.js
+++ controllerOne.js
@@ -3,8 +3,12 @@ var app = angular.module('jedModule');
 
 app.controller('controllerOne', ['$scope', 'jsonService', function($scope, jsonService) {
 
+	var theTitles = [];
 
 	jsonService.then(function(success) {
-		console.log(success.data);
+		for (var i = 0; i <15; i++) {
+		theTitles.push(success.data.data.children[i].data.title);
+		}
 	});
+	$scope.redditTitles = theTitles
 }]);
