42102174db97764650878f0b26123df63859765a
Edwin A <ea976@nyu.edu>
2016-03-09 13:58:38 -0500

    Moved directive to front page

--- directiveOne.js
+++ directiveOne.js
@@ -1,9 +1,10 @@
 var app = angular.module('jedModule');
 
 app.directive('directiveOne', function() {
-	return {
-	restrict: 'EA',
-	template: '<h4>EveryBody Look at me!</h4>',
-	replace: false
-	}
+	var vm = this
+	vm.restrict = 'EA';
+	vm.template= '<h4>EveryBody Look at me!</h4>';
+	vm.replace= false;
+	return vm;
+	
 });
